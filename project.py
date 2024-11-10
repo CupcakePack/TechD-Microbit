bded7af04430359a53746aee6625840b67441048
CupcakePack <owji359@gmail.com>
2024-11-10 13:50:14 +1100

    Can now drop chips, turn alternates
    Full chip dropping implemented
    Limited in flashing animation -> choppy
    Cannot detect win
    Turn alternation

--- project.py
+++ project.py
@@ -22,9 +22,9 @@ def check_winner():  # function to check winner
     pass
 
 
-def find_height(board, col):  # function to find the topmost empty cell in a column
+def find_height(game, col):  # function to find the lowest empty cell in a column
     for i in range(4, -1, -1):
-        if int(board[col][i]) == 0:
+        if int(game[i][col]) == 0:
             return i
 
 
@@ -32,9 +32,9 @@ choice = 0  # 0 - undecided, 1 - single player, 2 - two player
 
 display.scroll("A-1P   B-2P  ", delay=120, wait=False, loop=True)  # show the prompt message
 while choice == 0:  # while the user is still undecided
-    if button_a.is_pressed():  # single player
+    if button_a.was_pressed():  # single player
         choice = 1
-    elif button_b.is_pressed():  # two player
+    elif button_b.was_pressed():  # two player
         choice = 2
 
 # GAME START!
@@ -53,27 +53,38 @@ if choice == 2:  # implementation for two player
              ["0", "0", "0", "0", "0"],
              ["0", "0", "0", "0", "0"]]  # game board as a 2D list
     board_image = Image()  # game board as an Image
-    column = 0  # currently selected column, 0 is leftmost
+    column = 2  # currently selected column, 0 is leftmost, starts in the middle
 
     # main game loop
     while not winner:  # while game is still going on
+        column = 2
         display.show(turn)  # show the current turn
-        sleep(900)
-        display.show(board_image)  # show the board state
+        sleep(900)  # wait
+        display.show(board_image)
         selected = False  # player has finished selecting a column?
         while not selected:
+            height = find_height(board, column)  # find the topmost empty cell
             if turn == 1:
-                display.set_pixel(column, find_height(board, column), 9)
+                display.set_pixel(column, height, 9)  # flash animation on
             else:
-                display.set_pixel(column, find_height(board, column), 5)
+                display.set_pixel(column, height, 5)  # flash animation on
+                
             if button_a.was_pressed():  # change column
-                display.set_pixel(column, find_height(board, column), 0)
+                display.set_pixel(column, height, 0)  # stop flashing current pixel
                 column += 1
                 if column >= 5:  # cycle back
                     column = 0
-            display.set_pixel(column, find_height(board, column), 0)
-            if button_b.was_pressed():
-                pass
+                height = find_height(board, column)
                     
-
-
+            display.set_pixel(column, height, 0)  # flash animation off
+            
+            if button_b.was_pressed():
+                if turn == 1:
+                    board[height][column] = "9"
+                else:
+                    board[height][column] = "5"
+                turn = 2 if turn == 1 else 1
+                selected = True
+                board_image = Image(str(':'.join(''.join(str(cell) for cell in row) for row in board)))
+                # construct the board in an image format
+                display.show(board_image)  # show the board state
